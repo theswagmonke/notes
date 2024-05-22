876dfaa1a5afbc5bafab68dba132f8a6d94f7bb3
Korshunov Maksim <theswagmonke@gmail.com>
2024-05-22 10:16:57 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:notes/splash_screen.dart';
+
+void main() => runZonedGuarded(
+      () => runApp(const App()),
+      (error, stackTrace) => print('Top level exception:\n$error,\n$stackTrace'),
+    );
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
